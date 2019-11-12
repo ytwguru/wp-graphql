00117700895cdcebbc959cb07a50bb7a236d2789
Yomi Toba <ytwguru@gmail.com>
2019-11-12 07:16:13 -0600

    Fix search user loader cursor bug

--- src/Data/Loader/UserLoader.php
+++ src/Data/Loader/UserLoader.php
@@ -67,10 +67,8 @@ class UserLoader extends AbstractDataLoader {
 		 */
 		foreach ( $keys as $key ) {
 			$user = get_user_by( 'id', $key );
-            if(!$user)
-                $user = get_user_by( 'id', 2 );
-
-			$all_users[ $user->ID ] = new User( $user );
+            if($user)
+                $all_users[ $user->ID ] = new User( $user );
 		}
 		return $all_users;
 
