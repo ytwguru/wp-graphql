87091cc1aa8bf2aa1ef0e246641985de240ce021
Yomi Toba <ytwguru@gmail.com>
2019-11-12 08:22:38 -0600

    Change sort criteria

--- src/Data/Connection/PostObjectConnectionResolver.php
+++ src/Data/Connection/PostObjectConnectionResolver.php
@@ -277,10 +277,8 @@ class PostObjectConnectionResolver extends AbstractConnectionResolver {
 			 * Don't order search results by title (causes funky issues with cursors)
 			 */
 			//$query_args['search_orderby_title'] = false;
-            //$query_args['orderby']              = 'relevance';
-            //$query_args['order']                = isset( $last ) ? 'ASC' : 'DESC';
-            if(isset($query_args['orderby']))
-                unset($query_args['orderby']);
+            $query_args['orderby']              = 'relevance';
+            $query_args['order']                = isset( $last ) ? 'ASC' : 'DESC';
 		}
 
 		/**
